7f50228145838ee2cd4ca607584366b95778a380
Raúl Naveiras <me@raulnaveiras.com>
2018-08-29 18:10:53 +0200

    Amend replication metrics to properly ignore nulls
    When pg_basebackup is running in stream mode, it opens a second
    connection to the server and starts streaming the transaction log in
    parallel while running the backup. In both connections (state=backup and
    state=streaming) the pg_log_location_diff is null
    
    Signed-off-by: Raúl Naveiras <me@raulnaveiras.com>

--- collector/stat_replication.go
+++ collector/stat_replication.go
@@ -19,12 +19,18 @@ WITH pg_replication AS (
        , client_addr
        , state
        , sync_state
-       , (case when pg_is_in_recovery() THEN pg_xlog_location_diff(pg_last_xlog_receive_location(), replay_location)::float
-                                        ELSE pg_xlog_location_diff(pg_current_xlog_location(), replay_location)::float end)
-                                        AS pg_xlog_location_diff
+       , ( CASE when pg_is_in_recovery()
+           THEN pg_xlog_location_diff(pg_last_xlog_receive_location(), replay_location)::float
+           ELSE pg_xlog_location_diff(pg_current_xlog_location(), replay_location)::float
+		   END
+	     ) AS pg_xlog_location_diff
     FROM pg_stat_replication
-     )
-SELECT * FROM pg_replication WHERE pg_xlog_location_diff != null /*postgres_exporter*/`
+)
+/* When pg_basebackup is running in stream mode, it opens a second connection
+to the server and starts streaming the transaction log in parallel while
+running the backup. In both connections (state=backup and state=streaming) the
+pg_log_location_diff is null and it requires to be exclude */
+SELECT * FROM pg_replication WHERE pg_xlog_location_diff IS NOT NULL /*postgres_exporter*/`
 )
 
 type statReplicationCollector struct {
