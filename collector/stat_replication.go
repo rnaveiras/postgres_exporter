458791ce4a80d94b2e6c5774ece417c09f7835c4
Andrew Morton <andrewmorton@gocardless.com>
2022-04-28 13:25:48 +0100

    Drop Postgres 9 support from stat_replication

--- collector/stat_replication.go
+++ collector/stat_replication.go
@@ -14,26 +14,6 @@ running the backup. In both connections (state=backup and state=streaming) the
 pg_log_location_diff is null and it requires to be excluded */
 const (
 	// Scrape query
-	statReplicationLag9x = `
-WITH pg_replication AS (
-  SELECT application_name
-       , client_addr
-       , state
-       , sync_state
-       , ( CASE when pg_is_in_recovery()
-           THEN pg_xlog_location_diff(pg_last_xlog_receive_location(), replay_location)::float
-           ELSE pg_xlog_location_diff(pg_current_xlog_location(), replay_location)::float
-		   END
-	     ) AS pg_xlog_location_diff
-	   , ( CASE WHEN pg_last_wal_receive_lsn() = pg_last_wal_replay_lsn()
-		 THEN 0
-		 ELSE EXTRACT (EPOCH FROM now() - pg_last_xact_replay_timestamp())
-		 END
-	   ) as pg_standby_wal_replay_lag
-    FROM pg_stat_replication
-)
-SELECT * FROM pg_replication WHERE pg_xlog_location_diff IS NOT NULL /*postgres_exporter*/`
-
 	statReplicationLag = `
 WITH pg_replication AS (
   SELECT application_name
@@ -107,66 +87,6 @@ func (c *statReplicationScraper) Name() string {
 }
 
 func (c *statReplicationScraper) Scrape(ctx context.Context, conn *pgx.Conn, version Version, ch chan<- prometheus.Metric) error {
-	if version.Gte(10) {
-		return c.scrapeNewerThan9x(ctx, conn, ch)
-	}
-	return c.scrape9x(ctx, conn, ch)
-}
-
-func (c *statReplicationScraper) scrape9x(ctx context.Context, conn *pgx.Conn, ch chan<- prometheus.Metric) error {
-	var rows pgx.Rows
-	var err error
-
-	rows, err = conn.Query(ctx, statReplicationLag9x)
-
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-
-	var applicationName, state, syncState string
-	var clientAddr net.IP
-	var pgXlogLocationDiff, pgStandbyWalReplayLag float64
-
-	for rows.Next() {
-		if err := rows.Scan(&applicationName,
-			&clientAddr,
-			&state,
-			&syncState,
-			&pgXlogLocationDiff,
-			&pgStandbyWalReplayLag); err != nil {
-
-			return err
-		}
-
-		// postgres_stat_replication_lag_bytes
-		ch <- prometheus.MustNewConstMetric(c.lagBytes,
-			prometheus.GaugeValue,
-			pgXlogLocationDiff,
-			applicationName,
-			clientAddr.String(),
-			state,
-			syncState)
-
-		// postgres_standby_wal_replay_lag_seconds
-		ch <- prometheus.MustNewConstMetric(c.standbyWalReplayLag,
-			prometheus.GaugeValue,
-			pgStandbyWalReplayLag,
-			applicationName,
-			clientAddr.String(),
-			state,
-			syncState)
-	}
-	err = rows.Err()
-	if err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
-func (c *statReplicationScraper) scrapeNewerThan9x(ctx context.Context, conn *pgx.Conn, ch chan<- prometheus.Metric) error {
 	var rows pgx.Rows
 	var err error
 
